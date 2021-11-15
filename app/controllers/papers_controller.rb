7540ead395d77b945f90fc3745659fc1f50b2583
Tamino Bauknecht <dev@tb6.eu>
2021-11-16 00:35:56 +0100

    implementation for #15

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -3,7 +3,11 @@ class PapersController < ApplicationController
 
   # GET /papers
   def index
-    @papers = Paper.all
+    if params[:year]
+      @papers = Paper.released_in_year(params[:year])
+    else
+      @papers = Paper.all
+    end
   end
 
   # GET /papers/1
--- app/models/paper.rb
+++ app/models/paper.rb
@@ -3,4 +3,5 @@ class Paper < ApplicationRecord
   validates :title, presence: true
   validates :venue, presence: true
   validates :year, presence: true, numericality: { only_integer: true }
+  scope :released_in_year, ->(year) { where("year = ?", year) }
 end
