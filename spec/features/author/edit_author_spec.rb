b5b674edde20708705eb748000b1907b73ef222d
Tamino Bauknecht <dev@tb6.eu>
2021-11-15 18:53:12 +0100

    failing test for #7

---
+++ spec/features/author/edit_author_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+describe "Edit author page", type: :feature do
+  it "should exist at 'edit_author_path' and render withour error" do
+    # https://guides.rubyonrails.org/routing.html#path-and-url-helpers
+    visit edit_author_path(0)
+  end
+end
