5b29ee90f33a7fc5fa128397c4d98a060c5acfdb
Tamino Bauknecht <dev@tb6.eu>
2021-11-15 22:23:34 +0100

    failing test for #11

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,11 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
-  before(:all) do
+  before(:each) do
     @paper = create(:paper)
   end
 
   it "should be valid" do
     expect(@paper).to be_valid
+  end
+
+  it "should be invalid without title" do
+    @paper.title = nil
+    expect(@paper).to be_invalid
+  end
+
+  it "should be invalid without venue" do
+    @paper.venue = nil
+    expect(@paper).to be_invalid
+  end
+
+  it "should be invalid without year" do
+    @paper.year = nil
+    expect(@paper).to be_invalid
+  end
+
+  it "should be invalid with non-numeric year" do
+    @paper.year = "x"
+    expect(@paper).to be_invalid
   end
 end
