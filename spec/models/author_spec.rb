78fe756ca223b047d6e4d3f6d7c547e1da594309
Tamino Bauknecht <dev@tb6.eu>
2021-11-15 12:28:55 +0100

    failing test for #3

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -3,13 +3,29 @@ require 'rails_helper'
 RSpec.describe Author, type: :model do
   before(:each) do
 #  before(:all)
-    @author = Author.new
+    @first_name = "A"
+    @last_name = "B"
+    @homepage = "https://amazing-homepage-of-a-b.erde"
+    @author = Author.new(first_name: @first_name, last_name: @last_name, homepage: @homepage)
   end
 
-  it "should be valid with valid attributes" do
-    @author.first_name = "A"
-    @author.last_name = "B"
-    @author.homepage = "https://amazing-homepage-of-a-b.erde"
+  it "should be valid" do
     expect(@author).to be_valid
+  end
+
+  it "should assign first name to first_name" do
+    expect(@author.first_name).to eq(@first_name)
+  end
+
+  it "should assign last name to last_name" do
+    expect(@author.last_name).to eq(@last_name)
+  end
+
+  it "should assign homepage to homepage" do
+    expect(@author.homepage).to eq(@homepage)
+  end
+
+  it "should concatenate first_name and last_name when calling name" do
+    expect(@author.name).to eq(helper.concat_strings(@first_name, @last_name))
   end
 end
